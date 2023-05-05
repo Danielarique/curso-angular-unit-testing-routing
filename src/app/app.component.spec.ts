541ed7fd23dcd76dcc15153b3a7ccd4737bb522e
Daniela Rique <drique@asesoftware.com>
2023-05-05 11:20:39 -0500

    Avances video 9

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,26 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { RouterLinkDirectiveStub, queryAllByDirective  } from 'src/testing';
+fdescribe('AppComponent', () => {
 
-xdescribe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        RouterLinkDirectiveStub
       ],
     }).compileComponents();
   });
 
+  beforeEach(()=>{
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  })
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'ng-testing-services'`, () => {
+/*   it(`should have as title 'ng-testing-services'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ng-testing-services');
@@ -31,5 +39,19 @@ xdescribe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('ng-testing-services app is running!');
-  });
+  }); */
+
+  it('should there are 7 routerLinks', ()=>{
+    const links = queryAllByDirective(fixture,RouterLinkDirectiveStub);
+    expect(links.length).toEqual(7)
+  })
+
+  it('should there are 7 routerLinks with match routes', ()=>{
+    const links = queryAllByDirective(fixture,RouterLinkDirectiveStub);
+    const routerLinks = links.map(link => link.injector.get(RouterLinkDirectiveStub));
+    expect(links.length).toEqual(7);
+    expect(routerLinks[0].linkParams).toEqual('/')
+    expect(routerLinks[1].linkParams).toEqual('/auth/register')
+
+  })
 });
